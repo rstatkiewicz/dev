19f5718c84b50a79231a883d19779206685cc7f4
Radoslaw Statkiewicz <radoslawstatkiewicz2@gmail.com>
2020-01-05 18:51:55 -0500

    feat: bem

---
+++ main.js
@@ -0,0 +1,10 @@
+console.log('hey');
+let name = "Radek";
+let age = 32; 
+let radek= null;
+console.log(name);
+console.log(age);
+
+console.log('my name is + ${name} + "my age is" + ${age}');
+
+console.log('welcome');
